586d916fa8d276b08e4419c6c4f4e09b800a0f40
Michael Israel <mich.m.israel@gmail.com>
2019-11-30 13:34:03 +0100

    Update test script.

--- src/Test/test.sh
+++ src/Test/test.sh
@@ -1,8 +1,13 @@
-../CallFollower.py -vv -d . follow up Child1 -l 10
-dot -Tpng -o parents.png Child1.gv 
+echo Testing follow up...
+time ../CallFollower.py -d . follow up Child1 -l 10
+dot -Tpng -o parents.png Child1.gv
 
-../CallFollower.py -vv -d . follow down Parent1
+echo
+echo Testing follow down...
+time ../CallFollower.py -d . follow down Parent1 -l 10
 dot -Tpng -o children.png Parent1.gv
 
-../CallFollower.py -vv -d . follow all Relative
+echo
+echo Testing follow all...
+time ../CallFollower.py -d . follow all Relative -l 10
 dot -Tpng -o all.png Relative.gv
