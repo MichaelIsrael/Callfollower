ffa913ba33d78310eb4c560e5388af0e619235d5
Michael Israel <mich.m.israel@gmail.com>
2020-03-22 11:29:40 +0100

    Only add parser if present.

--- src/CodeParser/__init__.py
+++ src/CodeParser/__init__.py
@@ -1,8 +1,12 @@
 from CodeParser.ParserType import ParserType
-from CodeParser.Cscope import Cscope
 
-
-ParserType.addType("Cscope", Cscope)
+# Note: the first added parser is the default one.
+try:
+    from CodeParser.Cscope import Cscope
+except Exception:
+    pass
+else:
+    ParserType.addType("Cscope", Cscope)
 
 
 __all__ = ["ParserType"]
