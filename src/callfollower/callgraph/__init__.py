0e2924c6fbf729a3b4ec66ca3e556f862c4a2bb3
Michael Israel <mich.m.israel@gmail.com>
2020-11-29 19:46:12 +0100

    rename module

--- src/callfollower/callgraph/__init__.py
+++ src/callfollower/callgraph/__init__.py
@@ -7,7 +7,7 @@ __author__ = "Michael Israel"
 
 
 from .callgraph import CallGraph, CallGraphNode, CallGraphEdge
-from .graphizgraphgenerator import PygraphvizGenerator
+from .pygraphvizgenerator import PygraphvizGenerator
 from enum import Enum
 
 
